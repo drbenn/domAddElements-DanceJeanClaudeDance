07430d82d9d0d39ff15490cea38578d92afe977e
ratlord <bennett.daniel@gmail.com>
2022-04-16 12:49:35 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const btn = document.querySelector('.btn');
+const btnClear = document.querySelector('.btnClear');
+const danceContainer = document.querySelector('.dance-container');
+
+// Create Elements and Add to div danceContainer
+const dance = function () {
+  console.log('Dance! JCVD! Dance!');
+  let newDanceDiv = document.createElement('div');
+  newDanceDiv.classList.add('danceDiv');
+  let newImage = document.createElement('img');
+  let randomNumber = Math.floor(Math.random() * 10) + 1;
+  let randomImage = `/img/dance${randomNumber}.gif`;
+  console.log(randomImage);
+  newImage.setAttribute('src', randomImage);
+  newDanceDiv.appendChild(newImage);
+  danceContainer.appendChild(newDanceDiv);
+};
+
+// Remove Populated Dance Images
+const clear = function () {
+  console.log('heeeyyy');
+  while (danceContainer.firstChild) {
+    danceContainer.removeChild(danceContainer.firstChild);
+  }
+};
+
+btn.addEventListener('click', dance);
+btnClear.addEventListener('click', clear);
